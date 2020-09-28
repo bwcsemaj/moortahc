7e4b0bf63b2eccc314775251b61ef52718eef41e
bwcsemaj <bwc_semaj@live.com>
2020-09-28 14:46:05 -0400

    removed temp cors settings

--- server.authenticate/src/main/java/com/moortahc/server/authenticate/AuthenticateDriver.java
+++ server.authenticate/src/main/java/com/moortahc/server/authenticate/AuthenticateDriver.java
@@ -18,22 +18,4 @@ public class AuthenticateDriver {
         SpringApplication.run(AuthenticateDriver.class, args);
     }
     
-    
-    @Bean
-    public CorsFilter corsFilter() {
-        final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        final CorsConfiguration config = new CorsConfiguration();
-        config.setAllowCredentials(true);
-        config.addAllowedOrigin("*");
-        config.addAllowedHeader("*");
-        config.addAllowedMethod("OPTIONS");
-        config.addAllowedMethod("HEAD");
-        config.addAllowedMethod("GET");
-        config.addAllowedMethod("PUT");
-        config.addAllowedMethod("POST");
-        config.addAllowedMethod("DELETE");
-        config.addAllowedMethod("PATCH");
-        source.registerCorsConfiguration("/**", config);
-        return new CorsFilter(source);
-    }
 }
